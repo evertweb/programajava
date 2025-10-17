14ffc7677b40bf9dd1ac4f0aa7afec1dff745d8c
evertjava <cotacto@evertcardenas.com>
2025-10-17 14:29:50 -0500

    checkpoint 1.4  finalizado

--- forestech-cli-java/src/main/java/com/forestech/Main.java
+++ forestech-cli-java/src/main/java/com/forestech/Main.java
@@ -35,6 +35,23 @@ public class Main {
         double totalMovement = subTotalMovement + iva;
         boolean isBigBuy = fuelQuantity >= 100;
         boolean isValidPurchase = isInput && fuelType.equals("DIESEL") && fuelQuantity > 0;
+        String movementType = "ENTRADA";
+        int menu = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         System.out.println("=================================");
@@ -46,18 +63,52 @@ public class Main {
         System.out.println("Base de datos: " + DATABASE);
         System.out.println("Estado: " + (ACTIVE ? "Activo" : "Inactivo"));
         System.out.println("=================================");
-        System.out.println(txtMovement);
-        System.out.println("TIPO DE MOVIMIENTO: " + typeMovement);
-        System.out.println("TIPO DE COMBUSTIBLE: " + fuelType);
-        System.out.println("CANTIDAD: " + fuelQuantity + " " + textpresentation);
-        System.out.println("=================================");
-        System.out.println(txtCalculo);
-        System.out.println("EL SUBTOTAL DE TU COMPRA ES " + String.format("%.2f", subTotalMovement));
-        System.out.println("EL IVA DE TU COMPRA ES " + String.format("%.2f", iva));
-        System.out.println("EL TOTAL DE TU COMPRA ES " + String.format("%.2f", totalMovement));
-        System.out.println("TU COMPRA FUE GRANDE " + isBigBuy);
-        System.out.println("LA COMPRA ES VALIDA: " + isValidPurchase);
-        System.out.println("=================================");
+//        System.out.println(txtMovement);
+//        System.out.println("TIPO DE MOVIMIENTO: " + typeMovement);
+
+        System.out.println("MENU DE OPCIONES");
+        System.out.println("1. Registrar entrada de combustible");
+        System.out.println("2. Registrar salida de combustible");
+        System.out.println("3. Consultar inventario de combustible");
+        System.out.println("4. Salir");
+        System.out.println("TU SELECCIÓN: " + menu);
+
+        switch (menu){
+            case 1:
+                System.out.println("Registrar entrada de combustible");
+                break;
+            case 2:
+                System.out.println("Registrar salida de combustible");
+                break;
+            case 3:
+                System.out.println("Consultar inventario de combustible");
+                break;
+            case 4:
+                System.out.println("Salir");
+                break;
+            default:
+                System.out.println("Opción inválida");
+        }
+        if (movementType.equals("ENTRADA")){
+            System.out.println("INVENTARIO AUMENTARA ");
+
+        } else if (movementType.equals("SALIDA")) {
+            System.out.println("INVENTARIO DISMINUIRÁ");
+
+        }else {
+            System.out.println("VALOR INVALIDO");
+        }
+
+//        System.out.println("TIPO DE COMBUSTIBLE: " + fuelType);
+//        System.out.println("CANTIDAD: " + fuelQuantity + " " + textpresentation);
+//        System.out.println("=================================");
+//        System.out.println(txtCalculo);
+//        System.out.println("EL SUBTOTAL DE TU COMPRA ES " + String.format("%.2f", subTotalMovement));
+//        System.out.println("EL IVA DE TU COMPRA ES " + String.format("%.2f", iva));
+//        System.out.println("EL TOTAL DE TU COMPRA ES " + String.format("%.2f", totalMovement));
+//        System.out.println("TU COMPRA FUE GRANDE " + isBigBuy);
+//        System.out.println("LA COMPRA ES VALIDA: " + isValidPurchase);
+//        System.out.println("=================================");
 
 
 
